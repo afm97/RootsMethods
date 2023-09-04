386ad44361c9ebe0581100fa3ec61e77ab0e19a4
afm97 <arthurfranca97@gmail.com>
2023-09-04 10:50:30 -0300

    Como a função mudou, mudou-se também a derivada

--- derivative.c
+++ derivative.c
@@ -1,5 +1,5 @@
 float derivative_function_calc(float x0){
     //ESTA FUNCAO CALCULA O VALOR DA DERIVADA DE UMA FUNCAO EM DETERMINADO PONTO X0
 
-    return(3*pow(x0,2) - 1);
+    return(18*pow(x0,2) - 50*x0 + 2);
 }
