a2a51d638d6e7944b156ee76dadeb09583255a15
afm97 <arthurfranca97@gmail.com>
2023-09-05 11:34:38 -0300

    Remocao da verificacao f(c) = 0 do loop

--- newton_raphson.c
+++ newton_raphson.c
@@ -1,8 +1,8 @@
 
-float newton_raphson(float x_0, int number_of_iterations, float acceptable_iterative_approach){
+float newton_raphson(float x_0, float root,int number_of_iterations, float acceptable_iterative_approach){
     //ESTA FUNCAO RECEBE UM PONTO INICIAL E A PARTIR DELE BUSCA O ZERO DE UMA DADA FUNÇÃO
     int iteration = 0;
-    float x_n, f, df, iterative_approach;
+    float x_n, f, df, iterative_approach, erro;
 
     f = function_calc(x_0); df = derivative_function_calc(x_0);
 
@@ -12,16 +12,17 @@ float newton_raphson(float x_0, int number_of_iterations, float acceptable_itera
 
     do{
         
+        iteration++;
+
         x_n = x_0 - f/df;
 
+        erro = root - x_n;
         iterative_approach = pow(pow((x_n - x_0)/x_n,2),0.5);
 
-        printf(" \titeration = %.3d |\terro = %.8f |\troot = %f |\tf(x) = %.5f \n", iteration,iterative_approach,x_n,f);
+        printf(" \titeration = %.3d |\terro de iteracao = %.10f |\terro = %.10f |\troot = %f |\tf(x) = %f \n", iteration,iterative_approach,erro,x_n,f);
 
         f = function_calc(x_n); df = derivative_function_calc(x_n);
         x_0 = x_n;
-
-        iteration++;
 
     } while (iteration < number_of_iterations && iterative_approach > acceptable_iterative_approach);
     
